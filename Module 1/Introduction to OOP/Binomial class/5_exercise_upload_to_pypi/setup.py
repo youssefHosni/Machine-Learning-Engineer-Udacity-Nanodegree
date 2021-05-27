8b64e0b157c520544d4f5265ce2c3b8c4af5edbd
Youssef Hosni <youssefraafat57@gmail.com>
2021-05-27 14:01:20 +0300

    upload to pypi setup

---
+++ Module 1/Introduction to OOP/Binomial class/5_exercise_upload_to_pypi/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dists_yh',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['dists_yh'],
+      zip_safe=False)
